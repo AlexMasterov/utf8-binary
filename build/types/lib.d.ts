c4858e35f166bd3a6a00e441e213378325bf19ef
Alex Masterov <alex.masterow@gmail.com>
2019-11-30 23:26:32 +0300

    Update types (0.3.0)

--- build/types/lib.d.ts
+++ build/types/lib.d.ts
@@ -1,12 +1,14 @@
-// Type definitions for @asm/utf8 0.2.0
+// Type definitions for @asm/utf8 0.3.0
 // Project: https://github.com/AlexMasterov/utf8.js
 // Definitions by: Alex Masterov <https://github.com/AlexMasterov>
 // TypeScript Version: 3.7
 
 declare module '@asm/utf8' {
-  export function getUtf8Len(bin: string, offset?: number, length?: number) : number;
+  type Binary = Array | Uint8Array | Buffer;
+
+  export function countUtf8(bin: Binary, offset?: number, length?: number) : number;
   export function strToUint8(str: string, offset?: number, length?: number) : Uint8Array;
   export function strToUtf8(str: string, offset?: number, length?: number) : string;
-  export function utf8ToStr(bin: string, offset?: number, length?: number): string;
-  export function utf8viewToStr(view: DataView, offset?: number, length?: number) : string;
+  export function utf8ToStr(bin: Binary, offset?: number, length?: number): string;
+  export function viewToStr(view: DataView, offset?: number, length?: number) : string;
 }
