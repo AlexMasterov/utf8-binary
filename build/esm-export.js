b5fe5efb10fd687be6ebe493a28fdafb02b22121
Alex Masterov <alex.masterow@gmail.com>
2019-11-30 23:26:16 +0300

    Update build exports

--- build/esm-export.js
+++ build/esm-export.js
@@ -9,31 +9,32 @@ const makeExportByDefault = (...targets) =>
   targets.reduce(reduceExports, {});
 
 module.exports = makeExportByDefault(
+  // index
+  'count_utf8.js',
   'str-uint8.js',
   'str-utf8.js',
   'utf8-str.js',
-  'utf8len.js',
-  'utf8view-str.js',
+  'view-str.js',
   // legacy (v8v45)
-  'legacy/make/str-uint8.js',
   'legacy/str-uint8.js',
   'legacy/str-utf8.js',
   'legacy/utf8-str.js',
-  'legacy/utf8view-str.js',
+  'legacy/view-str.js',
+  'legacy/make/str-uint8.js',
   // make
   'make/chr.js',
   'make/chr1.js',
   'make/chr1_str.js',
+  'make/chr2.js',
   'make/cpr.js',
   'make/empty.js',
   'make/str-uint8.js',
-  'make/utf2.js',
   // chrs
-  'chrs/utf2.js',
   'chrs/chr1.js',
   'chrs/chr1_str.js',
+  'chrs/chr2.js',
   // chrs/make
-  'chrs/make/utf2.js',
   'chrs/make/chr1.js',
   'chrs/make/chr1_str.js',
+  'chrs/make/chr2.js',
 );
